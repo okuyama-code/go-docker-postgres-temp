c0f0215dd09414ee930a91086d33d3f79f36c707
okuyama-code <ooyy0121@gmail.com>
2024-06-23 18:51:17 +0900

    postmanでregisterとloginのエンドポイントを叩けた。DBに登録もされている。がが空欄でも登録されるため修正の必要あり。

--- README.md
+++ README.md
@@ -16,12 +16,64 @@ docker compose up
 サンプルプログラムで動作の確認
 app コンテナに入る
 ```
-docker-compose exec app sh
+docker compose exec app bash
 ```
 ```
 go run main.go
 ```
-Testingと出たら動作している
+postmanなどを使ってこれらを叩いてみる
+http://localhost:8080/register
+http://localhost:8080/login
+
+
+メソッド: POST
+URL: http://localhost:8080/register
+http://localhost:8080/login こっちも同じように。
+
+
+ヘッダー設定:
+
+Key: Content-Type
+Value: application/json
+
+
+ボディ設定:
+
+「raw」を選択し、JSONフォーマットを指定
+以下のJSONを入力:
+```
+{
+  "username": "testuser",
+  "password": "testpassword123"
+}
+```
+
+送信ボタンをクリックしてリクエストを送信
+
+
+期待される応答:
+/register
+
+```
+{
+    "ID": 2,
+    "CreatedAt": "2024-06-23T09:46:28.614542586Z",
+    "UpdatedAt": "2024-06-23T09:46:28.614542586Z",
+    "DeletedAt": null,
+    "username": "okuyama22",
+    "password": "$2a$10$g0TVCV/A3Jfv2dKtVaQVXuA5y/TDC.SIeYmoLJSSDi16SeXY3TX/W"
+}
+```
+
+/loginの成功は
+```
+成功の場合 (HTTPステータスコード 200):
+jsonCopy{
+  "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
+}
+```
+
+
 
 ## postgresの中に入ってテーブルなどを確認するために
 ```
@@ -30,7 +82,7 @@ docker compose up
 
 db コンテナに入る
 ```
-docker-compose exec db bash
+docker compose exec db bash
 ```
 コンテナ内のシェルに入ったら、PostgreSQLのコマンドラインツールであるpsqlを使用してデータベースに接続します。
 psql -U <ユーザー名> -d <データベース名>
@@ -43,10 +95,16 @@ psql -U postgres -d react_go_app
 ```
 \dt
 ```
-特定のテーブルを確認
+特定のテーブルを確認　\d <テーブル名>　カラムなどを見れる
+
 ```
-\d <テーブル名>
+\d users
 ```
+テーブルのデータを確認
+```
+SELECT * FROM users;
+```
+
 
 PostgreSQLのプロンプトを終了する \はoption + ¥
 ```
--- app/main.go
+++ app/main.go
@@ -1,7 +1,121 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Username string `gorm:"type:varchar(100);unique_index" json:"username"`
+	Password string `gorm:"type:varchar(100)" json:"password"`
+}
+
+var DB *gorm.DB
 
 func main() {
-	fmt.Println("Testing")
+	fmt.Println("main.goが呼ばれました")
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dbHost := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbPort := os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Tokyo",
+		dbHost, dbUser, dbPassword, dbName, dbPort)
+
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("failed to connect database: %v", err)
+	}
+
+	sqlDB, err := DB.DB()
+	if err != nil {
+		log.Fatalf("failed to get database: %v", err)
+	}
+	defer sqlDB.Close()
+
+	DB.AutoMigrate(&User{})
+
+	r := gin.Default()
+
+	r.POST("/register", register)
+	r.POST("/login", login)
+
+	r.Run()
+}
+
+func register(c *gin.Context) {
+	var user User
+	if err := c.BindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while hashing password"})
+		return
+	}
+	user.Password = string(hashedPassword)
+
+	if err := DB.Create(&user).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while registering user"})
+		return
+	}
+	c.JSON(http.StatusOK, user)
+}
+
+func login(c *gin.Context) {
+	var user User
+	if err := c.BindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var foundUser User
+	if err := DB.Where("username = ?", user.Username).First(&foundUser).Error; err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid username or password"})
+		return
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(foundUser.Password), []byte(user.Password)); err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid username or password"})
+		return
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"username": user.Username,
+		"exp":      time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	jwtSecret := os.Getenv("JWT_SECRET")
+	if jwtSecret == "" {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "JWT Secret not found"})
+		return
+	}
+
+	tokenString, err := token.SignedString([]byte(jwtSecret))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while generating token"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"token": tokenString})
 }
